9598b85acc22b411e08791c715598808a51c44c4
He Lihui <mistletoe997@163.com>
2021-01-14 16:34:22 +0800

    firse commit

---
+++ generators/app/index.js
@@ -0,0 +1,62 @@
+const Generator = require('yeoman-generator')
+
+module.exports = class extends Generator {
+    prompting() {
+        this.log(`
+        .__                               
+        __  _  __ ____ |  |   ____  ____   _____   ____  
+        \ \/ \/ // __ \|  | _/ ___\/  _ \ /     \_/ __ \ 
+         \     /\  ___/|  |_\  \__(  <_> )  Y Y  \  ___/ 
+          \/\_/  \___  >____/\___  >____/|__|_|  /\___  >
+                     \/          \/            \/     \/ 
+   Have fun.
+        `)
+        return this.prompt([{
+                type: 'input',
+                name: 'name',
+                message: 'Your project name:',
+                default: this.appname
+            }])
+            .then(answers => {
+                this.answers = answers
+            })
+    }
+    writing() {
+        const templates = [
+            '.browserslistrc',
+            '.editorconfig',
+            '.env.development',
+            '.env.production',
+            '.eslintrc.js',
+            '.gitignore',
+            'babel.config.js',
+            'package.json',
+            'postcss.config.js',
+            'README.md',
+            'public/favicon.ico',
+            'public/index.html',
+            'src/App.vue',
+            'src/main.js',
+            'src/router.js',
+            'src/assets/logo.png',
+            'src/components/HelloWorld.vue',
+            'src/store/actions.js',
+            'src/store/getters.js',
+            'src/store/index.js',
+            'src/store/mutations.js',
+            'src/store/state.js',
+            'src/utils/request.js',
+            'src/views/About.vue',
+            'src/views/Home.vue'
+        ]
+
+        templates.forEach(item => {
+            // item => 每个文件路径
+            this.fs.copyTpl(
+                this.templatePath(item),
+                this.destinationPath(item),
+                this.answers
+            )
+        })
+    }
+}
